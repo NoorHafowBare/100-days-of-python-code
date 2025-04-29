278f35e8e39fa54b3456bfb793c6e993dc78f297
noor <noorhafowbare@gmail.com>
2025-04-29 13:41:15 +0300

    initial commit

---
+++ project_band_name_generator.py
@@ -0,0 +1,7 @@
+print("welcme to the band name generator")
+city = input("which city did you grow up?")
+print(city)
+pet = input("what is the name of your pet?")
+print(pet)
+
+print("your name could be:" + city + " " + pet)
