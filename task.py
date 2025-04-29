629729e4d51ac31aed8192b71343dc667ee96b76
noor <noorhafowbare@gmail.com>
2025-04-29 13:31:14 +0300

    initial commit

---
+++ task.py
@@ -0,0 +1,24 @@
+print("Hello world!\nHello world\nHello world")
+
+# string concatination
+print("Hello" + " " + "Noor")
+
+#input function
+# print("Hello " + input("what is your name?") + "!")
+
+#python variables
+# name = input("what is pets name?")
+# print(name)
+
+# name = "isse"
+# print(len(name))
+
+# print(len(input("what is your name?")))
+
+username = input("what is your name?")
+length = len(username)
+
+print(length)
+
+
+
