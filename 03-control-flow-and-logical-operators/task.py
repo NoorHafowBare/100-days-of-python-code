34ef1ba6bd62ce4e46cbb3da929a8d2d17168c84
NoorHafowBare <noorhafowbare@gmail.com>
2025-05-17 09:11:48 +0300

    initial commit

---
+++ 03-control-flow-and-logical-operators/task.py
@@ -0,0 +1,27 @@
+#control flow and logical operators
+print("welcome to the rollercoster")
+height = int(input("what is your height in cm? "))
+
+if height >= 120:
+    print("you can ride roleercoster? ")
+else:
+    print("sorry you have to grow taller before you can ride")
+
+#modulo operator
+number_to_check = int(input("what is the number you need to check "))
+
+if number_to_check % 2 == 0:
+    print("this is even number")
+    age = int(input("what is your age "))
+    if age <= 12:
+        print("please pay 5$")
+    elif age <=18:
+        print("please pay the 7$")
+    else:
+        print("please pay the 7$")
+else:
+    print("this is and odd number")
+
+# nested if statements and elif statements
+
+
